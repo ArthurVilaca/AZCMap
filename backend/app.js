aac6c56c6ced65348657283aba9d1897647f81ae
wagoid <w.rsantos@outlook.com>
2016-05-08 09:25:22 -0300

    Change backend and add node_modules to gitignore

--- backend/app.js
+++ backend/app.js
@@ -5,25 +5,32 @@ var config = require('./config/environment');
 var http = require('http');
 var mongoose = require('mongoose');
 
-//Use node's default Promise with then and catch as the mongoose promise system
-mongoose.Promise = global.Promise;
-// Connect to MongoDB
-mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-  console.error('MongoDB connection error: ' + err);
-  process.exit(-1);
-});
+function makeServer() {
+  //Use node's default Promise with then and catch as the mongoose promise system
+  mongoose.Promise = global.Promise;
+  // Connect to MongoDB
+  mongoose.connect(config.mongo.uri, config.mongo.options);
+  mongoose.connection.on('error', function (err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+  });
 
-var app = express();
-var server = http.Server(app);
-var io = require('socket.io')(server);
-//set the socket io configuratio
-require('./config/socketio')(io);
+  var app = express();
+  var server = http.Server(app);
+  var io = require('socket.io')(server);
+  //set the socket io configuratio
+  require('./config/socketio')(io);
 
-require('./config/express')(app);
-require('./routes')(app);
+  require('./config/express')(app);
+  require('./routes')(app);
 
-//config.ip won't be used in production
-server.listen(config.port, function(){
-  console.log('listening on *:%d, in %s mode', config.port, config.env);
-});
+  //config.ip won't be used in production
+  server.listen(config.port, function () {
+    console.log('listening on *:%d, in %s mode', config.port, config.env);
+  });
+  
+  return server;
+}
+
+//Exports the makeServer function to improve testability
+module.exports = makeServer;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var app = require('./app');
+
 // Set default node environment to development
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
 // Export the application
-module.exports = require('./app');
+module.exports = app.makeServer();
